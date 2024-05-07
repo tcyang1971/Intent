6266e0309ade6293a489a9e1a0b891db8e11af10
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-05-07 15:11:24 +0800

    v2

--- app/src/main/java/tw/edu/pu/csim/tcyang/intent/SecondActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/intent/SecondActivity.kt
@@ -1,11 +1,9 @@
 package tw.edu.pu.csim.tcyang.intent
 
-import android.content.Intent
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -13,14 +11,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.intent.ui.theme.IntentTheme
 
-class MainActivity : ComponentActivity() {
+class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,8 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //Greeting()
-                    FirstScreen()
+                    Greeting("Android")
                 }
             }
         }
@@ -39,22 +34,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun FirstScreen(){
+fun Greeting(name: String, modifier: Modifier = Modifier) {
     val context = LocalContext.current  //取得App的運行環境
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Cyan),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    )
-    {
-        Button(onClick = {
-            var it = Intent(context, SecondActivity::class.java)
-            context.startActivity(it)
-        })
+    val activity = (context as Activity)  //取得App運行的活動
+
+    Column {
+        Text(
+            text = "SecondAcyivity"
+        )
+
+        Button(
+            onClick = {
+                activity.finish()
+            })
         {
-            Text(text = "跳轉到SecondActivity")
+            Text(text = "回到MainActivity")
         }
     }
 }
-
